ce03085ea195b3c19cede3fede3e4b50e5ad3fab
egoing <egoing@gmail.com>
2020-01-11 18:53:14 +0900

    HTML CODING

--- src/App.js
+++ src/App.js
@@ -5,20 +5,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+      <header>
+        <h1>WEB</h1>
+        World Wide WEB
       </header>
+
+      <nav>
+        <ol>
+          <li><a href="html.html">HTML</a></li>
+          <li><a href="css.html">CSS</a></li>
+          <li><a href="javascript.html">JavaScript</a></li>
+        </ol>
+      </nav>
+
+      <article>
+        <h2>Welcome</h2>
+        Hello, WEB.
+      </article>
+
     </div>
   );
 }
